c5b05f7f35aab0e52d8a0b437ef38a636ab17e58
Ivan Tat <ivtat3048@gmail.com>
2025-12-29 02:07:49 -0500

    first commit

---
+++ src/test/java/com/zk/store/StoreApplicationTests.java
@@ -0,0 +1,15 @@
+package com.zk.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
