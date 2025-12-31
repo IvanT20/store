3a83f131f76cbbd4128f77c69c4ef80f04c54cac
Ivan Tat <ivtat3048@gmail.com>
2025-12-31 01:50:14 -0500

    -Added more tables (profiles, user_tags, and tags) to database

---
+++ src/main/resources/db/migration/V3__add_profile_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE profiles
+(
+    id              BIGINT PRIMARY KEY,
+    bio             TEXT,
+    phone_number    VARCHAR(15),
+    date_of_birth   DATE,
+    loyalty_points  INT UNSIGNED DEFAULT 0,
+    CONSTRAINT profiles_users_id_fk
+        FOREIGN KEY (id) REFERENCES users (id)
+);
---
+++ src/main/resources/db/migration/V4__add_tags_and_user_tags.sql
@@ -0,0 +1,5 @@
+CREATE TABLE tags
+(
+    id      BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name    VARCHAR(255) NOT NULL
+);
